8e81338fb90fa77564f287730110bad37a3bea8a
Brianpanas <brianpanasgaming@gmail.com>
2025-03-14 05:01:21 +0800

    Added DashboardCardRB
    Add DashboardCardRB Pade. Also updated Rostrap Rules

---
+++ Module Bryan/Pages/Page_Dark Mode.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dark Mode Toggle</title>
+    <link rel="stylesheet" href="../Styles/DarkMode_RBS.css">
+</head>
+<body>
+
+        <!-- Dark mode -->
+        <button
+            id="darkModeToggleRB"
+            onclick="controller_Btn_DarkMode()"
+            title="Toggle light/dark mode"
+            data-light-img="../src/sun.png"
+            data-dark-img="../src/light-mode.png"
+            style="background-image: url('../src/sun.png'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+        </button>
+        <!-- Dark mode -->
+
+    <script type="module" src="../Client Side/Controller_Btn_DarkMode.js"></script>
+    <script type="module"  src="../Client Side/Controller_Btn_ButtonRB.js"></script>
+
+</body>
+</html>
