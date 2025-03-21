65c1b5924dba50ee6fc76fbf560cd58b1ebf34c3
Brianpanas <brianpanasgaming@gmail.com>
2025-03-22 05:30:28 +0800

    Rivision of VPT
    Vertical Progress Tracker Revision

--- Module Bryan/Pages/Page_Vertical Progress Tracker.php
+++ Module Bryan/Pages/Page_Vertical Progress Tracker.php
@@ -4,47 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vertical Progress Tracker</title>
-    <link rel="stylesheet" href="../Styles/RBPT Vertical Progress Tracker.css">
+    <link rel="stylesheet" href="./CSS/RBPT Vertical Progress Tracker.css">
 </head>
-
 <body>
 
     <!-- Vertical Progress Tracker -->
     <div class="rbpt-container">
-        <div class="rbpt-line"></div>
-
+        <!-- Apply the .rbpt-line-last class here -->
+        <div class="rbpt-line rbpt-line-last"></div> <!-- Line for the last step -->
+        
         <div class="rbpt-step">
+            <div class="rbpt-time">2025-03-22 10:00 AM</div>
             <img src="./src/checkIcon.png" alt="Tracker">
             <div class="rbpt-text">Order Placed</div>
         </div>
 
         <div class="rbpt-step">
+            <div class="rbpt-time">2025-03-22 12:30 PM</div>
             <img src="./src/checkIcon.png" alt="Tracker">
             <div class="rbpt-text">Shipped</div>
         </div>
 
         <div class="rbpt-step">
+            <div class="rbpt-time">2025-03-22 04:00 PM</div>
             <img src="./src/checkIcon.png" alt="Tracker">
             <div class="rbpt-text">Out for Delivery</div>
         </div>
 
         <div class="rbpt-step">
+            <div class="rbpt-time">2025-03-22 05:00 PM</div>
             <img src="./src/checkIcon.png" alt="Tracker">
-            <div class="rbpt-text">add here</div>
+            <div class="rbpt-text">Add Here</div>
         </div>
 
         <div class="rbpt-step">
+            <div class="rbpt-time">2025-03-22 06:00 PM</div>
             <img src="./src/checkIcon.png" alt="Tracker">
-            <div class="rbpt-text">add here</div>
+            <div class="rbpt-text">Add Here</div>
         </div>
 
         <div class="rbpt-step">
+            <div class="rbpt-time">2025-03-22 07:30 PM</div>
             <img src="./src/complete icon purple.png" alt="Progress">
             <div class="rbpt-text rbpt-last-step">Delivered</div>
         </div>
     </div>
     <!-- Vertical Progress Tracker -->
-    
-</body>
 
+</body>
 </html>
