4162e5dcb5da65a0615ed156a3c73d8caacef49f
veretheragna <esmedinaasuncion75@gmail.com>
2025-03-25 09:09:19 -0700

    Update the Page profile
    modify the html of the page profile dropdown

--- Module Ariel/Pages/Page_ProfileDropDown.php
+++ Module Ariel/Pages/Page_ProfileDropDown.php
@@ -16,10 +16,11 @@
 		<div class="dropdown_ArFilter" onclick="controller_Click_DropDown()">
 			<div class="dropdown-button_ArFilter pic_ArFilter" id="dropdownButton_ArFilter" style="--profileDropDownArrowDownImg: url('../../src/ArrowDown.png')"></div>
 		    <!-- Dropdown list items-->
-		    <div class="menu_ArFilter" id="optionsMenu_ArFilter">
+			<div class="menu_ArFilter" id="optionsMenu_ArFilter">
 		    	<a href="" class="menu-item_ArFilter dottedUnderline"><img src="../../src/EditProfile.png" class="ar_SubPic">Edit Profile</a>
 		    	<a href="" class="menu-item_ArFilter dottedUnderline"><img src="../../src/gear_purple.png" class="ar_SubPic">Settings</a>
 		    	<a href="" class="menu-item_ArFilter"><img src="../../src/logouticon black.png" class="ar_SubPic">Logout</a>
+			</div>
 		</div>
 	</div>	
 
