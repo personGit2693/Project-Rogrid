088ee3732715bc2e0543e79601d2ce1f1aae01ed
Brianpanas <brianpanasgaming@gmail.com>
2025-04-06 21:52:36 +0800

    Toggle Button RB Added
    New Component

---
+++ Module Bryan/Pages/Toggle Button RB.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Toggle Button RB</title>
+  <link rel="stylesheet" href=".././Styles/Toggle Button RB.css">
+</head>
+<body>
+
+  <label class="toggle-switch-RB">
+    <input type="checkbox" class="toggle-input-RB">
+    <div class="slider-RB">
+      <span class="icon-RB check-RB">✔</span>
+      <span class="icon-RB cross-RB">✖</span>
+    </div>
+  </label>
+
+</body>
+</html>
