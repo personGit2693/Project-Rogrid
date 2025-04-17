44a0d97c614e56391390a9470cbb1a96859b8a30
veretheragna <esmedinaasuncion75@gmail.com>
2025-04-17 08:10:57 -0700

    Update Executor_Page_ProfileDropDown.js

--- Module Ariel/Client Side/Executor_Page_ProfileDropDown.js
+++ Module Ariel/Client Side/Executor_Page_ProfileDropDown.js
@@ -6,6 +6,7 @@ import { ar_ProfiledDropdownContent } from './Element_Page_ProfileDropDown.js';
 
 /*Dependencies*/
 document.addEventListener("click", () => {
+    
     for(let index= 0; index < ar_ProfiledDropdownContent.length; index++) {
         ar_ProfiledDropdownContent[index].style.display = "none";
     }
