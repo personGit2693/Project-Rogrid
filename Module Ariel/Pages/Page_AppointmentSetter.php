131fae52bd829779677b75aa88c212d31ec39863
Brianpanas <esmedinaasuncion75@gmail.com>
2025-05-25 17:57:45 +0800

    hi
    Update

---
+++ Module Ariel/Pages/Page_AppointmentSetter.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Appointment Setter</title>
+	<link rel="stylesheet" type="text/css" href="../Styles/Page_AppointmentSetter.css">
+</head>
+<body>
+
+	<div class="ar_ASetter">
+		<div class="ar_ASetterDate">Appointment Date<input type="date" class="ar_Date"></div>
+		<div>Monday</div>
+		<div>Tuesday</div>
+		<div>Wednesday</div>
+		<div>Thrusday</div>
+		<div>Friday</div>
+		<div>Saturday</div>
+		<div>Sunday</div>
+	</div>
+
+</body>
+</html>
