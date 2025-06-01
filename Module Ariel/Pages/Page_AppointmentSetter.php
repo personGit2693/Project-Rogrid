ed4a074436bea75b0f69675eea2ef5ccd741f4d9
veretheragna <esmedinaasuncion75@gmail.com>
2025-06-01 09:42:48 -0700

    Sample Setter
    Sample appointment setter

--- Module Ariel/Pages/Page_AppointmentSetter.php
+++ Module Ariel/Pages/Page_AppointmentSetter.php
@@ -8,16 +8,133 @@
 </head>
 <body>
 
+<div class="sampleContainer">
 	<div class="ar_ASetter">
 		<div class="ar_ASetterDate">Appointment Date<input type="date" class="ar_Date"></div>
-		<div>Monday</div>
-		<div>Tuesday</div>
-		<div>Wednesday</div>
-		<div>Thrusday</div>
-		<div>Friday</div>
-		<div>Saturday</div>
-		<div>Sunday</div>
+
+		<div class="ar_ASTime">
+			<div class="ar_Days">Monday</div>
+			<div class="ar-AppointmentTime">Morning
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">9:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">10:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">11:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">12:00</button>
+				</div>
+				Noon
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">1:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">2:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">3:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">4:00</button>
+				</div>
+			</div>
+		</div>
+
+	
+		<div class="ar_ASTime">
+			<div class="ar_Days">Tuesday </div>
+			<div class="ar-AppointmentTime">Morning
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">9:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">10:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">11:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">12:00</button>
+				</div>
+				Noon
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">1:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">2:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">3:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">4:00</button>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="ar_ASTime">
+			<div class="ar_Days">Wednesday</div>
+			<div class="ar-AppointmentTime">Morning
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">9:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">10:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">11:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">12:00</button>
+				</div>
+				Noon
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">1:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">2:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">3:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">4:00</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="ar_ASTime">
+			<div class="ar_Days">Thursday</div>
+			<div class="ar-AppointmentTime">Morning
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">9:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">10:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">11:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">12:00</button>
+				</div>
+				Noon
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">1:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">2:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">3:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">4:00</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="ar_ASTime">
+			<div class="ar_Days">Friday</div>
+			<div class="ar-AppointmentTime">Morning
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">9:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">10:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">11:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">12:00</button>
+				</div>
+				Noon
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">1:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">2:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">3:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">4:00</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="ar_ASTime">
+			<div class="ar_Days">Saturday</div>
+			<div class="ar-AppointmentTime">Morning
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">9:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">10:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">11:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">12:00</button>
+				</div>
+				Noon
+				<div class="ar_subAppointmentTime">
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">1:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">2:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">3:00</button>
+					<button class="ar-individualTime" onclick="controller_Btn_HLTime(this)">4:00</button>
+				</div>
+			</div>
+		</div>
 	</div>
 
+</div>
+
+
+
+
+	<script type="module" src="../Client Side/Controller_Btn_HLTime.js"></script>
+	
 </body>
 </html>
