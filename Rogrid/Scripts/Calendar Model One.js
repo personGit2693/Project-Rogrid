91dbf445292909a707858790d623bac9539ed8cc
Rogiero <Cruzjr.rogelio@gmail.com>
2025-06-30 10:59:12 +0800

    Calendar Model One JS updated

--- Rogrid/Scripts/Calendar Model One.js
+++ Rogrid/Scripts/Calendar Model One.js
@@ -95,7 +95,7 @@ function rendercmo(cmo_totaldays, cmo_month, cmo_year, cmo_rg_id){
 		cmo_div_day.addEventListener("click", function(){
 
 			/*Editable, you can add more event when date was selected*/
-			assigncmoValue(cmo_year, cmo_month+1, this, calLite, cmo_value_rg);			
+			assigncmoValue(cmo_year, cmo_month+1, this, cmo_rg, cmo_value_rg);			
 			/*Editable, you can add more event when date was selected*/
 
 			/*For Date Range only*/
@@ -114,38 +114,41 @@ function rendercmo(cmo_totaldays, cmo_month, cmo_year, cmo_rg_id){
 
 
 		/***Setting Attribute per day number div*/
-		let attriMonth_Rogrid = cmo_month+1;
-		if(attriMonth_Rogrid < 10){
-			attriMonth_Rogrid = "0"+attriMonth_Rogrid;
+		let data_cmo_month = cmo_month+1;
+		if(data_cmo_month < 10){
+			data_cmo_month = "0"+data_cmo_month;
 		}
 
-		attriDay_Rogrid = index;
-		if(attriDay_Rogrid < 10){
-			attriDay_Rogrid = "0"+attriDay_Rogrid;
+		data_cmo_day = index;
+		if(data_cmo_day < 10){
+			data_cmo_day = "0"+data_cmo_day;
 		}
-		cmo_div_day.setAttribute("data-dayDate", cmo_year+"-"+attriMonth_Rogrid+"-"+attriDay_Rogrid);
+
+		const data_cmo_date = `${cmo_year}-${data_cmo_month}-${data_cmo_day}`;
+		cmo_div_day.setAttribute("data-cmo-date", data_cmo_date);
 		/***Setting Attribute per day number div*/		
-
 		
-		const perDate_Rogrid = cmo_year+"-"+attriMonth_Rogrid+"-"+attriDay_Rogrid;
-		if(cmo_filterdate_rg_array.includes(perDate_Rogrid)){
-			/*Editable, change font color and bg color when perDate is on filtered date*/
+		
+		if(cmo_filterdate_rg_array.includes(data_cmo_date)){
+
+			/*Editable, change font color and bg color when data_cmo_date is on cmo_filterdate_rg_array*/
 			cmo_div_day.style.backgroundColor = "green";
 			cmo_div_day.style.color = "#ffffff";
-			/*Editable, change font color and bg color when perDate is on filtered date*/
-		}else if(!cmo_filterdate_rg_array.includes(perDate_Rogrid)){
-			/*Editable, change font color and bg color when perDate is not on filtered date*/
+			/*Editable, change font color and bg color when data_cmo_date is on cmo_filterdate_rg_array*/
+		}else if(!cmo_filterdate_rg_array.includes(data_cmo_date)){
+
+			/*Editable, change font color and bg color when data_cmo_date is not on cmo_filterdate_rg_array*/
 			cmo_div_day.style.backgroundColor = "transparent";
 			cmo_div_day.style.color = "black";
-			/*Editable, change font color and bg color when perDate is not on filtered date*/
+			/*Editable, change font color and bg color when data_cmo_date is not on cmo_filterdate_rg_array*/
 		}		
 
 
-		const createdDate_Rogrid = monthTxt_Rogrid+" "+index+", "+cmo_year;		
+		const cmo_date = `${cmo_months_array[cmo_month]} ${index}, ${cmo_year}`;		
 
-		const dayOfWeek_Rogrid = new Date(createdDate_Rogrid).getDay();
+		const cmo_date_day = new Date(cmo_date).getDay();
 
-		cmo_days_rg.children[dayOfWeek_Rogrid].appendChild(cmo_div_day);
+		cmo_days_rg.children[cmo_date_day].appendChild(cmo_div_day);
 	}
 	/**Creating days per week loop*/
 }
@@ -153,31 +156,38 @@ function rendercmo(cmo_totaldays, cmo_month, cmo_year, cmo_rg_id){
 
 
 /*Function to Change calendar lite month*/
-function changeCalLiteMonth(addMinusMonth_Param, cmo_rg_id){
-	const calLite = document.getElementById(cmo_rg_id);
-	const cmo_month_rg = cmo_rg.querySelector(".cmo_month_rg_RoClass");
-	const cmo_year_rg = cmo_rg.querySelector(".cmo_year_rg_RoClass");
+function changecmoMonth(cmo_month_ctrl, cmo_rg_id){
 
-	let cmo_month_rg_Int = parseInt(cmo_month_rg.value);
-	let cmo_year_rg_Int = parseInt(cmo_year_rg.value);
+	const cmo_rg = document.getElementById(cmo_rg_id);
+	const cmo_month_rg = cmo_rg.querySelector(".cmo_month_rg");
+	const cmo_year_rg = cmo_rg.querySelector(".cmo_year_rg");
 
-	if(addMinusMonth_Param === "minus"){
-		if(cmo_month_rg_Int !== 0){
-			cmo_month_rg_Int -= 1;
-			rendercmo(getTotalDays(cmo_month_rg_Int, cmo_year_rg_Int), cmo_month_rg_Int, cmo_year_rg_Int, cmo_rg_id);
-		}else if(cmo_month_rg_Int === 0){
-			cmo_month_rg_Int = 11;
-			cmo_year_rg_Int -= 1;
-			rendercmo(getTotalDays(cmo_month_rg_Int, cmo_year_rg_Int), cmo_month_rg_Int, cmo_year_rg_Int, cmo_rg_id);
+	let cmo_month = parseInt(cmo_month_rg.value);
+	let cmo_year = parseInt(cmo_year_rg.value);
+
+	if(cmo_month_ctrl === "back"){
+
+		if(cmo_month !== 0){
+
+			cmo_month -= 1;
+			rendercmo(getTotalDays(cmo_month, cmo_year), cmo_month, cmo_year, cmo_rg_id);
+		}else if(cmo_month === 0){
+
+			cmo_month = 11;
+			cmo_year -= 1;
+			rendercmo(getTotalDays(cmo_month, cmo_year), cmo_month, cmo_year, cmo_rg_id);
 		}
-	}else if(addMinusMonth_Param === "add"){
-		if(cmo_month_rg_Int !== 11){
-			cmo_month_rg_Int += 1;
-			rendercmo(getTotalDays(cmo_month_rg_Int, cmo_year_rg_Int), cmo_month_rg_Int, cmo_year_rg_Int, cmo_rg_id);
-		}else if(cmo_month_rg_Int === 11){
-			cmo_month_rg_Int = 0;
-			cmo_year_rg_Int += 1;
-			rendercmo(getTotalDays(cmo_month_rg_Int, cmo_year_rg_Int), cmo_month_rg_Int, cmo_year_rg_Int, cmo_rg_id);
+	}else if(cmo_month_ctrl === "next"){
+
+		if(cmo_month !== 11){
+
+			cmo_month += 1;
+			rendercmo(getTotalDays(cmo_month, cmo_year), cmo_month, cmo_year, cmo_rg_id);
+		}else if(cmo_month === 11){
+
+			cmo_month = 0;
+			cmo_year += 1;
+			rendercmo(getTotalDays(cmo_month, cmo_year), cmo_month, cmo_year, cmo_rg_id);
 		}
 	}
 }
@@ -185,55 +195,65 @@ function changeCalLiteMonth(addMinusMonth_Param, cmo_rg_id){
 
 
 /*Function to Change calendar lite year*/
-function changeCalLiteYear(addMinusYear_Param, cmo_rg_id){
-	const calLite = document.getElementById(cmo_rg_id);
-	const cmo_month_rg = cmo_rg.querySelector(".cmo_month_rg_RoClass");
-	const cmo_year_rg = cmo_rg.querySelector(".cmo_year_rg_RoClass");
+function changecmoYear(cmo_month_ctrl, cmo_rg_id){
 
-	let cmo_month_rg_Int = parseInt(cmo_month_rg.value);
-	let cmo_year_rg_Int = parseInt(cmo_year_rg.value);
+	const cmo_rg = document.getElementById(cmo_rg_id);
+	const cmo_month_rg = cmo_rg.querySelector(".cmo_month_rg");
+	const cmo_year_rg = cmo_rg.querySelector(".cmo_year_rg");
 
-	if(addMinusYear_Param === "minus"){
-		cmo_year_rg_Int -= 1;
-		rendercmo(getTotalDays(cmo_month_rg_Int, cmo_year_rg_Int), cmo_month_rg_Int, cmo_year_rg_Int, cmo_rg_id);		
-	}else if(addMinusYear_Param === "add"){
-		cmo_year_rg_Int += 1;
-		rendercmo(getTotalDays(cmo_month_rg_Int, cmo_year_rg_Int), cmo_month_rg_Int, cmo_year_rg_Int, cmo_rg_id);		
+	let cmo_month = parseInt(cmo_month_rg.value);
+	let cmo_year = parseInt(cmo_year_rg.value);
+
+	if(cmo_month_ctrl === "back"){
+
+		cmo_year -= 1;
+		rendercmo(getTotalDays(cmo_month, cmo_year), cmo_month, cmo_year, cmo_rg_id);		
+	}else if(cmo_month_ctrl === "next"){
+
+		cmo_year += 1;
+		rendercmo(getTotalDays(cmo_month, cmo_year), cmo_month, cmo_year, cmo_rg_id);		
 	}
 }
 /*Function to Change calendar lite year*/
 
 
-/*Function for clicking days on calender lite*/ 
-function assigncmoValue(calLiteYearVal_Param, calLiteMonth_Param, dayNumDivElem_Param, cmo_rg_id, cmo_value_rg_Param){	
-	if(calLiteMonth_Param < 10){
-		calLiteMonth_Param = "0"+calLiteMonth_Param;
+/*Function for clicking days on calender lite*/
+function assigncmoValue(cmo_year, cmo_month, cmo_div_day, cmo_rg, cmo_value_rg){
+
+	if(cmo_month < 10){
+		cmo_month = "0"+cmo_month;
 	}
 
-	calLiteDay_Param = dayNumDivElem_Param.innerText;
-	if(calLiteDay_Param < 10){
-		calLiteDay_Param = "0"+calLiteDay_Param;
+	let cmo_day = cmo_div_day.innerText;
+	if(cmo_day < 10){
+		cmo_day = "0"+cmo_day;
 	}
 
-	cmo_value_rg_Param.value = calLiteYearVal_Param+"-"+calLiteMonth_Param+"-"+calLiteDay_Param;
+	cmo_value_rg.value = `${cmo_year}-${cmo_month}-${cmo_day}`;
 	
 
 	/**Coloring bg and font selected day*/
-	const dayNumber_Rogrid_Coll = cmo_rg_id.querySelectorAll(".cmo_day_rg");
-	for(let index=0; index<dayNumber_Rogrid_Coll.length; index++){
-		if(dayNumber_Rogrid_Coll[index] == dayNumDivElem_Param){
+	const cmo_day_rg_array = cmo_rg.querySelectorAll(".cmo_day_rg");
+
+	for(let index=0; index<cmo_day_rg_array.length; index++){
+
+		if(cmo_day_rg_array[index] == cmo_div_day){
+
 			/*Editable, change font color and bg color of the selected date*/
-			dayNumber_Rogrid_Coll[index].style.backgroundColor = "#44D3A3"; 
-			dayNumber_Rogrid_Coll[index].style.color = "#ffffff";
+			cmo_day_rg_array[index].style.backgroundColor = "#44D3A3"; 
+			cmo_day_rg_array[index].style.color = "#ffffff";
 			/*Editable, change font color and bg color of the selected date*/
-		}else if(dayNumber_Rogrid_Coll[index] != dayNumDivElem_Param){
+		}else if(cmo_day_rg_array[index] != cmo_div_day){
+
 			/*Editable, logic and action for not selected date*/
-			if(cmo_filterdate_rg_array.includes(dayNumber_Rogrid_Coll[index].getAttribute("data-dayDate"))){
-				dayNumber_Rogrid_Coll[index].style.backgroundColor = "green";
-				dayNumber_Rogrid_Coll[index].style.color = "#ffffff";
+			if(cmo_filterdate_rg_array.includes(cmo_day_rg_array[index].getAttribute("data-cmo-date"))){
+
+				cmo_day_rg_array[index].style.backgroundColor = "green";
+				cmo_day_rg_array[index].style.color = "#ffffff";
 			}else{
-				dayNumber_Rogrid_Coll[index].style.backgroundColor = "transparent";
-				dayNumber_Rogrid_Coll[index].style.color = "black";
+
+				cmo_day_rg_array[index].style.backgroundColor = "transparent";
+				cmo_day_rg_array[index].style.color = "black";
 			}
 			/*Editable, logic and action for not selected date*/					
 		}
